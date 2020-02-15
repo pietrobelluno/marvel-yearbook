5a13ccf1935c02e038034d9d0f3e3e7f3113d8dc
Pietro Belluno Pilau <pietrop@outlook.com>
2020-02-15 17:13:35 -0300

    change api call in node

--- backend/index.js
+++ backend/index.js
@@ -3,32 +3,33 @@ const axios = require("axios");
 
 const app = express();
 
-app.get("/books", async (req, res) => {
+const limit = 20;
+const ts = "thesoer";
+const apiKey = "001ac6c73378bbfff488a36141458af2";
+const hash = "72e5ed53d1398abb831c3ceec263f18b";
+
+app.get("/page/:page", async (req, res) => {
+  const { page } = req.params;
   let offset = 0;
-  const limit = 2;
-  if (req.query.page && req.query.page > 1) {
-    const page = req.query.page;
+  if (page && page > 1) {
     offset = (page - 1) * limit;
   }
-  const ts = "thesoer";
-  const apiKey = "001ac6c73378bbfff488a36141458af2";
-  const hash = "72e5ed53d1398abb831c3ceec263f18b";
   const apiResponse = await axios.get(
     `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${apiKey}&hash=${hash}&offset=${offset}&limit=${limit}`
   );
 
   const results = apiResponse.data.data.results;
-  const data = [];
+  const characters = [];
   results.map(char => {
     const obj = {
       id: char.id,
       name: char.name,
       image: `${char.thumbnail.path}/portrait_xlarge.${char.thumbnail.extension}`
     };
-    data.push(obj);
+    characters.push(obj);
   });
-
-  return res.json({ data });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  return res.json({ characters });
 });
 
 app.listen(3333);
