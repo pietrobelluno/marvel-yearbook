649fbe9293d88d9cfe87940862082f33b4a088fa
Pietro Belluno Pilau <pietrop@outlook.com>
2020-02-15 13:14:21 -0300

    create the pagination with offset and limit

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,32 @@ const axios = require("axios");
 
 const app = express();
 
-app.get("/", async (request, response) => {
+app.get("/books", async (req, res) => {
+  let offset = 0;
+  const limit = 2;
+  if (req.query.page && req.query.page > 1) {
+    const page = req.query.page;
+    offset = (page - 1) * limit;
+  }
   const ts = "thesoer";
-  const apiKey =
-    "001ac6c73378bbfff488a36141458af2&hash=72e5ed53d1398abb831c3ceec263f18b";
+  const apiKey = "001ac6c73378bbfff488a36141458af2";
+  const hash = "72e5ed53d1398abb831c3ceec263f18b";
   const apiResponse = await axios.get(
-    `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${apiKey}`
+    `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${apiKey}&hash=${hash}&offset=${offset}&limit=${limit}`
   );
 
-  console.log(apiResponse.data.data.results);
-  return response.json({ message: "Hello World" });
+  const results = apiResponse.data.data.results;
+  const data = [];
+  results.map(char => {
+    const obj = {
+      id: char.id,
+      name: char.name,
+      image: `${char.thumbnail.path}/portrait_xlarge.${char.thumbnail.extension}`
+    };
+    data.push(obj);
+  });
+
+  return res.json({ data });
 });
 
 app.listen(3333);
