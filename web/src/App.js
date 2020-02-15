812b679aafff43a5be9e0a2d474ac15c84acb193
Pietro Belluno Pilau <pietrop@outlook.com>
2020-02-15 17:37:07 -0300

    install history and create pagination

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ListChars from "./pages/ListChars";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import history from "./history";
 function App() {
   return (
-    <Router>
+    <Router history={history}>
       <Switch>
         <Route path="/page/:page">
           <ListChars />
--- web/src/pages/ListChars.js
+++ web/src/pages/ListChars.js
@@ -3,32 +3,52 @@ import api from "../services/api";
 import "./style.css";
 import logoMarvel from "../images/marvel-logo.svg";
 import { useParams } from "react-router-dom";
+import history from "../history";
 
 function ListChars() {
   const { page } = useParams();
   const [characters, setCharacters] = useState([]);
+  const [pageParam, setPageParam] = useState(page ? page : 1);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     async function loadCharacters() {
-      const params = `page/${page ? page : 1}`;
+      setLoading(true);
+      const params = `page/${pageParam ? pageParam : 1}`;
       const res = await api.get(`/${params}`);
       setCharacters(res.data.characters);
+      setLoading(false);
     }
     loadCharacters();
   }, []);
 
-  async function nextPage() {
-    const res = await api.get(`/page/2`);
+  async function nextPage(page) {
+    setLoading(true);
+    history.push(`/page/${page}`);
+    const res = await api.get(`/page/${page}`);
     setCharacters(res.data.characters);
+    setPageParam(page);
+    setLoading(false);
+  }
+
+  async function prevPage(page) {
+    setLoading(true);
+    history.push(`/page/${page}`);
+    const res = await api.get(`/page/${page}`);
+    setCharacters(res.data.characters);
+    setPageParam(page);
+    setLoading(false);
   }
   return (
     <main>
-      {characters.length === 0 && (
+      {loading && (
         <div className="loading">
           <img src={logoMarvel} alt="Logo da Marvel" />
         </div>
       )}
       <ul className="wrapper">
         {characters &&
+          !loading &&
           characters.map(char => (
             <li className="charItem" key={char.id}>
               <img src={char.image} alt={char.name} />
@@ -36,8 +56,12 @@ function ListChars() {
             </li>
           ))}
         <div className="actions">
-          <button>Anterior</button>
-          <button onClick={() => nextPage()}>Próximo</button>
+          <button
+            onClick={() => prevPage(pageParam <= 1 ? pageParam : pageParam - 1)}
+          >
+            Anterior
+          </button>
+          <button onClick={() => nextPage(pageParam + 1)}>Próximo</button>
         </div>
       </ul>
     </main>
